3ec1bb0c7b8737aed12f8562f2f4037bc2d3a61f
akshayauti <akshayauti3260@gmail.com>
2019-03-09 11:17:01 +0530

    my portfolio first commit

---
+++ PersonalWebsite/portfolio/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, "portfolio/index.html")
+
+def contact(request):
+    return render(request, "portfolio/contact.html")
+
+
+def about(request):
+    return render(request, "portfolio/about.html")
+
+
+def work(request):
+    return render(request, "portfolio/work.html")
